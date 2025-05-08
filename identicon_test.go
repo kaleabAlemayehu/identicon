f1f5641f77def0b89613ab536e66c17deb0dba60
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-05-08 18:06:34 -0400

    feat: adding testing it may help for example how to use it.

--- identicon_test.go
+++ identicon_test.go
@@ -1,12 +1,29 @@
 package identicon
 
 import (
+	"os"
 	"testing"
 )
 
 func TestHash(t *testing.T) {
-	testCase0 := "kaleab is gonna get hashed kaleab is gonna get hashed kaleab is gonna get hashed kaleab is gonna get hashed"
-	testCase1 := "idfjdkfjadlkfjal kfajsflkajsflkajfla skfjdklfjakldfjalkdjfsdklfjalfa"
-	render(testCase0)
-	render(testCase1)
+	testCase0 := []byte("kaleab is gonna get hashed kaleab is gonna get hashed kaleab is gonna get hashed kaleab is gonna get hashed")
+	testCase1 := []byte("idfjdkfjadlkfjal kfajsflkajsflkajfla skfjdklfjakldfjalkdjfsdklfjalfa")
+	img, err := os.Create("./test0.png")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	icon := New7X7()
+
+	_, err = img.Write(icon.Render(testCase0))
+	if err != nil {
+		t.Error(err.Error())
+	}
+	img, err = os.Create("./test1.png")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	_, err = img.Write(icon.Render(testCase1))
+	if err != nil {
+		t.Error(err.Error())
+	}
 }
